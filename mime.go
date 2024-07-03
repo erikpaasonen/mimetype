717926891479d0f640c1c0791b636d4831ee31c7
Erik Paasonen <erik_github@paasonens.info>
2024-07-03 09:21:41 -0400

    Update mime.go

--- mime.go
+++ mime.go
@@ -3,8 +3,8 @@ package mimetype
 import (
 	"mime"
 
-	"github.com/gabriel-vasile/mimetype/internal/charset"
-	"github.com/gabriel-vasile/mimetype/internal/magic"
+	"github.com/erikpaasonen/mimetype/internal/charset"
+	"github.com/erikpaasonen/mimetype/internal/magic"
 )
 
 // MIME struct holds information about a file format: the string representation
